db6456eb335ddf669d464391456eaba8194e5ef3
JuraJuki <juraypavlovic@gmail.com>
2022-09-01 15:21:23 +0200

    change endpoint

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -12,7 +12,7 @@ export function Form() {
       return;
     }
 
-    const res = await fetch('http://localhost:51568/.netlify/functions/count', {
+    const res = await fetch('https://webcampsibeniktst.netlify.app/.netlify/functions/count', {
       method: 'POST',
       body: JSON.stringify({ count }),
     }).then((res) => res.json());
